0af67b4b802677f9f31641ea3b47803747d559f9
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-05-17 15:54:21 +0300

    done xd

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@ let page = document.querySelector(".page");
 let popup = page.querySelector(".popup");
 let editButton = page.querySelector(".profile__edit");
 let closeButton = page.querySelector(".popup__close-button");
-let editName = page.querySelector(".popup__input-name");
-let editProfileDescription = page.querySelector(".popup__input-description");
+let editName = page.querySelector(".popup__input_name");
+let editProfileDescription = page.querySelector(".popup__input_description");
 let profileName = page.querySelector(".profile__name");
 let profileDescription = page.querySelector(".profile__description");
 let submitButton = page.querySelector(".popup__submit-button");
@@ -20,7 +20,7 @@ function popupClose() {
 }
 
 function popupSubmitClose(event) {
-  event.preventDefault()
+  event.preventDefault();
   profileName.textContent = editName.value;
   profileDescription.textContent = editProfileDescription.value;
   popupClose();
@@ -29,4 +29,3 @@ function popupSubmitClose(event) {
 editButton.addEventListener("click", pupupOpen);
 closeButton.addEventListener("click", popupClose);
 form.addEventListener("submit", popupSubmitClose);
-
