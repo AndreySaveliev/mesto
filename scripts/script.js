ad94c3b4a2fbf2d03cbc1fa89d1e7d4bd8a887d0
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-06-16 15:08:07 +0300

    6spritn/first iter

--- scripts/script.js
+++ scripts/script.js
@@ -20,14 +20,19 @@ const popupShowCard = page.querySelector(".popup-card-section");
 const grid = page.querySelector(".grid");
 const cell = page.querySelector("#cell").content;
 const profileEdit = page.querySelector(".popup-profile-edit");
-const popupCardSection = page.querySelector(".popup-card-section");
 const linkNewCardInput = page.querySelector(".popup__input_card_link");
 const nameNewCardInput = page.querySelector(".popup__input_card_name");
 const closeCardButton = page.querySelector(".popup-card__close-button-card");
+const popupProfileEditForm = page.querySelector("#popup__profile-edit");
+const inputs = page.querySelectorAll(".popup__input");
+const popupForms = page.querySelectorAll(".popup__form");
+const cardBlock = page.querySelector(".popup-card__block");
+const profileBlock = page.querySelector(".popup-profile-block");
+const popupCardBlock = page.querySelector(".popup-card-add-block");
+const submitButtons = page.querySelectorAll(".popup__submit-button");
+const errors = page.querySelectorAll(".form__input-error");
 
 // ЗАНАЧЕНИЯ ПОПАПА РЕД. ПРОФИЛЯ
-
-// ВЫБОР ПОПАПА
 const setValuesToProfileForm = () => {
   editName.value = profileName.textContent;
   editProfileDescription.value = profileDescription.textContent;
@@ -36,12 +41,24 @@ const clearValuesToNewCardForm = () => {
   linkNewCardInput.value = "";
   nameNewCardInput.value = "";
 };
+
 // ОТКРЫТИЕ ПОПАПА И ЗАКРЫТИЕ
 const openPopup = (popup) => {
   popup.classList.add("popup_opened");
 };
+
 const closePopup = (popup) => {
   popup.classList.remove("popup_opened");
+  errors.forEach((error) => {
+    error.textContent = "";
+  });
+  inputs.forEach((input) => {
+    input.classList.remove("popup__input_show_error");
+  });
+  submitButtons.forEach((button) => {
+    button.removeAttribute("disable", true);
+    button.classList.remove("popup__submit-button-disable");
+  });
 };
 
 profileEditButton.addEventListener("click", () => {
@@ -52,11 +69,43 @@ addCardButton.addEventListener("click", () => {
   clearValuesToNewCardForm();
   openPopup(popupCardAdd);
 });
-
+// ЗАКРЫТИЕ ПО КЛИКУ НА КРЕСТИК
 profileEditClose.addEventListener("click", () => closePopup(profileEdit));
 addCardCloseButton.addEventListener("click", () => closePopup(popupCardAdd));
 closeCardButton.addEventListener("click", () => closePopup(popupShowCard));
+// ЗАКРЫТИЕ ПО НАЖАТИЮ НА ОВЕРЛЕЙ
+popupShowCard.addEventListener("mousedown", function (e) {
+  if (e.offsetX > cardBlock.offsetWidth || e.offsetY > cardBlock.offsetHeight) {
+    closePopup(popupShowCard);
+  }
+});
 
+profileEdit.addEventListener("mousedown", function (e) {
+  if (
+    e.offsetX > profileBlock.offsetWidth ||
+    e.offsetY > profileBlock.offsetHeight
+  ) {
+    closePopup(profileEdit);
+  }
+});
+
+popupCardAdd.addEventListener("mousedown", function (e) {
+  if (
+    e.offsetX > popupCardBlock.offsetWidth ||
+    e.offsetY > popupCardBlock.offsetHeight
+  ) {
+    closePopup(popupCardAdd);
+  }
+});
+//ЗАКРЫТЕ ПОПАП ПО ESC
+document.addEventListener("keydown", (evt) => {
+  if (evt.keyCode == 27) {
+    closePopup(popupShowCard);
+    closePopup(profileEdit);
+    closePopup(popupCardAdd);
+  }
+});
+// ЗНАЧЕНИЕ ИМЕНИ И ОПИСАНИЯ ПРОФИЛЯ В ПОПАПЕ
 page
   .querySelector("#popup__profile-edit")
   .addEventListener("submit", (event) => {
@@ -73,7 +122,7 @@ const createCard = (card) => {
   newCard.querySelector(".grid__name").textContent = card.name;
   newCard.querySelector(".grid__img").addEventListener("click", (event) => {
     openCard(event);
-    openPopup(popupCardSection);
+    openPopup(popupShowCard);
   });
   newCard.querySelector(".grid__like").addEventListener("click", likeCard);
   newCard.querySelector(".grid__delete").addEventListener("click", deleteCard);
@@ -120,4 +169,65 @@ const openCard = (event) => {
     event.target.nextElementSibling.textContent.trim();
   popupShowCard.querySelector(".popup-card__title").textContent =
     event.target.nextElementSibling.textContent;
+};
+
+// ВАЛИДАЦИЯ ФОРМЫ ПРОФИЛЯ
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("popup__input_show_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error-active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("popup__input_show_error");
+  errorElement.classList.remove("form__input-error-active");
+  errorElement.textContent = "";
+};
+
+const formEventListener = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+  const buttonElement = formElement.querySelector(".popup__submit-button");
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  popupForms.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    formEventListener(formElement);
+  });
+};
+
+enableValidation();
+// ИЗМИНЕНИЕ КНОПКИ ЕСЛИ ПОЛЯ НЕ ПРОШЛИ ВАЛИДАЦИЮ
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.setAttribute("disabled", true);
+    buttonElement.classList.add("popup__submit-button-disable");
+  } else {
+    buttonElement.removeAttribute("disabled ", true);
+    buttonElement.classList.remove("popup__submit-button-disable");
+  }
 };
